ed3c0247df8c5fa09f0b3ce823240b85ab64237b
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2024-10-05 16:05:26 +0200

    Add tests

---
+++ src/test/java/org/semver4j/internal/ComparatorTest.java
@@ -0,0 +1,20 @@
+package org.semver4j.internal;
+
+import org.junit.jupiter.api.Test;
+import org.semver4j.Semver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComparatorTest {
+    @Test
+    void shouldUnevenComparisonNotCrashAndBeCorrect() {
+        Semver semver1 = Semver.coerce("4.0.0-beta.9-macro");
+        Semver semver2 = Semver.coerce("4.0.0-beta.9-macro2");
+
+        assertThat(semver1).isNotNull();
+        assertThat(semver2).isNotNull();
+
+        assertThat(Comparator.compareTo(semver1, semver2)).isNegative();
+        assertThat(Comparator.compareTo(semver2, semver1)).isPositive();
+    }
+}
