8e73771b438e21ff21c58a416c90f5fc8e033727
Allen Ding <ading@palantir.com>
2024-10-10 15:49:02 -0400

    fixing pipeline test

--- src/test/java/org/semver4j/internal/range/RangeProcessorPipelineTest.java
+++ src/test/java/org/semver4j/internal/range/RangeProcessorPipelineTest.java
@@ -1,23 +1,40 @@
 package org.semver4j.internal.range;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
+import org.semver4j.internal.range.processor.Processor;
+
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RangeProcessorPipelineTest {
-//    @Test
-//    void shouldBeAbleToBuildAPipeline() {
-//        RangeProcessorPipeline pipeline = RangeProcessorPipeline.startWith(range -> range + "_A")
-//                .addProcessor(range -> range + "_B")
-//                .addProcessor(range -> range + "_C");
-//        assertThat(pipeline.process("RANGE")).isEqualTo("RANGE_A");
-//    }
-//
-//    @Test
-//    void shouldReturnPassedInStringIfNoProcessorIsSuccessful() {
-//        RangeProcessorPipeline pipeline = RangeProcessorPipeline.startWith(range -> null)
-//                .addProcessor(range -> null)
-//                .addProcessor(range -> null);
-//        assertThat(pipeline.process("RANGE")).isEqualTo("RANGE");
-//    }
+    @Test
+    void shouldBeAbleToBuildAPipeline() {
+        RangeProcessorPipeline pipeline = RangeProcessorPipeline.startWith(new DummyProcessor(range -> range + "_A"))
+                .addProcessor(new DummyProcessor(range -> range + "_B"))
+                .addProcessor(new DummyProcessor(range -> range + "_C"));
+        assertThat(pipeline.process("RANGE")).isEqualTo("RANGE_A");
+    }
+
+    @Test
+    void shouldReturnPassedInStringIfNoProcessorIsSuccessful() {
+        RangeProcessorPipeline pipeline = RangeProcessorPipeline.startWith(new DummyProcessor(range -> null))
+                .addProcessor(new DummyProcessor(range -> null))
+                .addProcessor(new DummyProcessor(range -> null));
+        assertThat(pipeline.process("RANGE")).isEqualTo("RANGE");
+    }
+
+    private final class DummyProcessor extends Processor {
+        private final Function<String, String> process;
+        private DummyProcessor(Function<String, String> process) {
+            this.process = process;
+        }
+
+        @Override
+        public @Nullable String tryProcess(@NotNull String range) {
+            return process.apply(range);
+        }
+    }
 }
