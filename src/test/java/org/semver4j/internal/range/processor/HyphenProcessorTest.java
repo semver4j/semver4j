a7ee091a109ed12f03de09ed7ed0aecd71e8f867
Allen Ding <ading@palantir.com>
2024-11-07 17:30:18 -0500

    update hyphen range

--- src/test/java/org/semver4j/internal/range/processor/HyphenProcessorTest.java
+++ src/test/java/org/semver4j/internal/range/processor/HyphenProcessorTest.java
@@ -41,12 +41,12 @@ class HyphenProcessorTest {
     static Stream<Arguments> shouldParseHyphenRangeIncludePrerelease() {
         return Stream.of(
                 arguments("1.2.3 - 2.3.4", ">=1.2.3 <2.3.5-0"),
-                arguments("1.2 - 2.3.4", ">=1.2.0 <2.3.5-0"),
-                arguments("1 - 2.3.4", ">=1.0.0 <2.3.5-0"),
+                arguments("1.2 - 2.3.4", ">=1.2.0-0 <2.3.5-0"),
+                arguments("1 - 2.3.4", ">=1.0.0-0 <2.3.5-0"),
                 arguments("1.2.3 - 2.3", ">=1.2.3 <2.4.0-0"),
                 arguments("1.2.3 - 2", ">=1.2.3 <3.0.0-0"),
                 arguments("1.2.3-alpha - 2.1.4-beta", ">=1.2.3-alpha <=2.1.4-beta"),
-                arguments("1.2 - 2.1.4-beta", ">=1.2.0 <=2.1.4-beta"),
+                arguments("1.2 - 2.1.4-beta", ">=1.2.0-0 <=2.1.4-beta"),
                 arguments("1.2.3-alpha - 2.1.4", ">=1.2.3-alpha <2.1.5-0"),
                 arguments("INVALID", null)
         );
