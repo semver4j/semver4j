c472901bd3160b8c5a681cfb129310b4fcb989cf
Piotr Olaszewski <piotroo89@gmail.com>
2025-05-01 08:17:23 +0200

    Update CoerceTest.java

--- src/test/java/org/semver4j/internal/CoerceTest.java
+++ src/test/java/org/semver4j/internal/CoerceTest.java
@@ -7,8 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CoerceTest {
     @Test
     void shouldRemoveLeadingZeros() {
+        // when
         String version = Coerce.coerce("1.08");
 
+        // then
         assertThat(version).isEqualTo("1.8.0");
     }
 }
